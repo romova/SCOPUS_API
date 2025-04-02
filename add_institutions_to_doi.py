315add8df4ffeebc79c81e33a4fa736908dd8241
Dominik Vladař <dominikvladar576@gmail.com>
2025-04-02 11:46:36 +0200

    more refs collected, using more api keys

--- add_institutions_to_doi.py
+++ add_institutions_to_doi.py
@@ -8,13 +8,20 @@ import os
 sys.stdout.reconfigure(encoding='utf-8')
 count = 0
 
+api_keys = [
+"LIST-YOUR-API-KEYS-HERE"]
+api_key_index = 0
+
+SCOPUS_HEADERS["X-ELS-APIKey"] = api_keys[0]
+
 
 def get_affiliation_to_ref(ref_json_object):
     global count
+    global api_key_index
+    global api_keys
     doi = ref_json_object["doi"]
     if doi == "N/A":
         return
-    time.sleep(0.5)
     url = f"https://api.elsevier.com/content/abstract/doi/{doi}"
 
     response = requests.get(url, headers=SCOPUS_HEADERS)
@@ -24,7 +31,18 @@ def get_affiliation_to_ref(ref_json_object):
 
     if response.status_code != 200:
         print(f"❌ API Request Failed: {response.status_code}")
+        if response.status_code == 429:
+            api_key_index += 1
+            if api_key_index >= len(api_keys):
+                print("❌ All API Keys have been used")
+                sys.exit(1)
+                return
+            SCOPUS_HEADERS["X-ELS-APIKey"] = api_keys[api_key_index]
+            print("🔑 Changing API Key")
+            get_affiliation_to_ref(ref_json_object)
+            return
         print(response.text)
+        return
 
     data = response.json()
     if "abstracts-retrieval-response" in data and "affiliation" in data["abstracts-retrieval-response"]:
@@ -32,7 +50,7 @@ def get_affiliation_to_ref(ref_json_object):
     else:
         ref_json_object["affiliation"] = "N/A"
 
-for file in [f"all_articles_by_institution_cited_{year}.json" for year in range(2007, 2026)]:
+for file in [f"all_articles_by_institution_cited_{year}.json"  for year in range(2020, 2022)]:
     if file.endswith(".json"):
         with open(f"data_by_year/{file}", "r", encoding="utf-8") as f:
             data = json.load(f)
@@ -45,4 +63,4 @@ for file in [f"all_articles_by_institution_cited_{year}.json" for year in range(
                         get_affiliation_to_ref(ref)
         with open(f"data_by_year/{file}", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
-        print(f"{file} has been updated with affiliations")   
+        print(f"{file} has been updated with affiliations")     
